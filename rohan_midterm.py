168defe846265b5e3c60199047b23672d430fa30
Rohan-Elahi <156772383+Rohan-Elahi@users.noreply.github.com>
2024-01-18 22:01:41 +0500

    Add files via upload

---
+++ rohan_midterm.py
@@ -0,0 +1,44 @@
+#Rohan Elahi
+#midterm exam
+
+#Q1 - Write a Python program to do arithmetical operations addition and division.
+
+#INPUT 1
+a=int(input("ENTER NUM 1 : "))
+#INPUT 2
+b=int(input("ENTER NUM 2 : "))
+sum=a+b
+print("THE SUM IS : ",sum )
+
+
+
+#INPUT 1
+a=int(input("ENTER NUM 1 : "))
+#INPUT 2
+b=int(input("ENTER NUM 2 : "))
+div=a/b
+print("THE DIVISION IS : ",div )
+
+
+#Q2 Write a Python program to find the area of a triangle.
+
+#INPUT 1
+a=int(input("ENTER THE LENGTH OF THE BASE OF TRIANGLE  : "))
+#INPUT 2
+b=int(input("THE HEIGHT OF THE TRIANGLE IS  : "))
+area=(1/2*b*a) 
+print("THE AREA OF TRIANGLE IS : ",area)
+
+#Q3 Write a Python program to convert Celsius to Fahrenheit.
+
+#INPUT 1
+a=int(input("ENTER TEMPRATURE IN CELSIUS  : "))
+#INPUT 2
+fahrenheit=(9/5*a+32)
+print("THE TEMPRATURE IN FAHRENHEIT : ",fahrenheit)
+
+#Q4 Write a Python program that return all datatypes that we discussed in the cl
+
+#INPUT 1
+a=int(input(type))
+print("THE DATA TYPE IS :",a)
